f6d1b8bf16e105c5e8ee676250387e72d3b73112
yyyn012 <yyyn12_@naver.com>
2024-08-07 15:39:51 +0900

    hot reloading(2)
    It does not work properly (because React 18 and React DOM conflict).
    
    but hot reloading is work.

--- react-webgame/lecture/webpack.config.js
+++ react-webgame/lecture/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const webpack = require("webpack");
 const RefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
 
 module.exports = {
@@ -31,7 +32,6 @@ module.exports = {
             "@babel/preset-react",
           ],
           plugins: [
-            "@babel/plugin-proposal-class-properties",
             "react-refresh/babel",
             //babel이 최신 문법을 적용할 뿐만 아니라 핫 로더 기능까지 추가해준다.
           ],
@@ -49,7 +49,17 @@ module.exports = {
   },
   //devServer 설정
   devServer: {
-    port: 9000,
+    devMiddleware: { publicPath: "/dist" },
+    static: { directory: path.resolve(__dirname) },
     hot: true,
   },
+  // webpack의 업로드로 인해 devServer설정 방식이 위와 같이 바뀜
+
+  // devServer => devMiddleware
+
+  // static이 추가되었다. static의 directory에는 __dirname과 index.html이 위치해있는 폴더이름을 적어준다.
+
+  //ex) static: { directory: path.resolve(__dirname, "src") },
+
+  // 만약 index.html이 같은 폴더에 있다면 위와 같이 __dirname만 적어주면 된다.
 };
