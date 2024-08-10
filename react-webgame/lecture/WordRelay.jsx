a56318c72e20c8dde547817723fcd7ecbf6e55ec
yyyn012 <yyyn12_@naver.com>
2024-08-10 16:49:06 +0900

    Change WordRelay from class to hooks

--- react-webgame/lecture/WordRelay.jsx
+++ react-webgame/lecture/WordRelay.jsx
@@ -1,61 +1,45 @@
 const React = require("react");
 const { Component } = React;
 
-class WordRelay extends Component {
-  state = {
-    word: "끝말잇기",
-    value: "",
-    result: "",
-  };
+const WordRelay = () => {
+  const [word, setWord] = useState("끝말잇기");
+  const [value, setValue] = useState("");
+  const [result, setResult] = useState("");
+  const inputRef = useRef(null);
 
-  onSubmit = () => {
+  const onSubmit = (e) => {
     e.preventDefault();
 
-    // 앞 단어의 마지막 글자와 현재 입력한 단어의 첫번째 글자가 같은 지 비교
-    if (this.state.word[this.state.word.length - 1] === this.state.value[0]) {
-      //같을 경우
-      this.setState({
-        result: "ok",
-        word: this.state.value,
-        value: "",
-      });
-      this.input.focus();
+    if (word[word.length - 1] === value[0]) {
+      setResult("ok");
+      setWord(value);
+      value("");
 
-      //같지 않을 경우
+      inputRef.current.focus();
+      //hooks에서는 Ref에 current를 항상 붙여야 한다.
     } else {
-      this.setState({
-        result: "game over",
-        value: "",
-      });
+      setResult("game over");
+      setValue("");
+      inputRef.current.focus();
     }
   };
 
-  onChange = (e) => {
-    this.setState({ value: e.target.value });
+  const onChange = (e) => {
+    setResult(e.target.value);
   };
 
-  input;
-
-  onRef = (c) => {
-    this.input = c;
-  };
-
-  render() {
-    return (
-      <div>
-        <div>{this.state.word}</div>
-        <form onSubmit={this.onSubmit}>
-          <input
-            ref={this.onRef}
-            value={this.state.value}
-            onChange={this.onChange}
-          />
-          <button>입력!</button>
-        </form>
-        <div>{this.state.result}</div>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <div>{word}</div>
+      <form onSubmit={onSubmit}>
+        <input ref={inputRef} value={value} onChange={onChange} />
+        <button>입력!</button>
+      </form>
+      <div>{result}</div>
+    </div>
+  );
+};
 
 module.exports = WordRelay;
+
+// 정상적으로 작동되었을 때 콘솔에 뜨는 [HMR]는 HotModule-Reload, [WDS]는 Webpack-Dev-Server이다.
