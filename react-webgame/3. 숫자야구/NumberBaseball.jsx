ea39d64d5713fad922db3a4be017ae9b3f6587f9
yyyn012 <yyyn12_@naver.com>
2024-08-20 15:05:24 +0900

    map() and key

--- react-webgame/3. 숫자야구/NumberBaseball.jsx
+++ react-webgame/3. 숫자야구/NumberBaseball.jsx
@@ -25,7 +25,6 @@ class NumberBaseball extends Component {
             maxLength={4}
             value={this.state.value}
             onChange={this.onChange}
-            //value와 onChange 둘 다 쓰고 싶지 않다면 이 둘 대신에 defaultValue={this.state.value}를 쓰면 된다.
           />
         </form>
         <div>try : {this.state.tries.length}</div>
@@ -34,22 +33,17 @@ class NumberBaseball extends Component {
             { fruit: "apple", taste: "good" },
             { fruit: "banana", taste: "sweet" },
             { fruit: "grape", taste: "favorite" },
-            // 2차원 배열보다는 보통 위와 같이 배열 안의 객체를 많이 사용한다.
-          ].map((v) => (
+            { fruit: "apple", taste: "not bad" },
+          ].map((v, i) => (
+            // 여기에서 i는 인덱스를 나타낸다. (v, 0)은 첫번째 객체를 뜻한다.
+            // i는 고유한 값(겹치지 않는 숫자)이라 종종 key에 i를 넣는 실수를 할 때가 있다.
+            // 하지만 이 경우 성능최적화에 문제가 생길 수 있기 때문에 key에는 i를 입력하지 않는 것이 좋다.
+            // (리액트에서 무엇이 바뀌었는지 인식하는 데 어려움이 있을 수 있다고 한다.)
             <li key={v.fruit + v.taste}>
-              <b>{v.fruit}</b>- {v.taste}
+              {/* v.fruit만 키 값으로 입력할 경우 마지막 객체에서 "apple"이 겹치기 때문에 v.fruit + v.taste를 키 값으로 입력해주었다. */}
+              <b>{v.fruit}</b>- {i}
             </li>
           ))}
-          {/* 화살표 함수에서 ()는 return을 뜻하기 때문에 return{}을 삭제해도 잘 돌아간다. */}
-
-          {/* 이 경우 v가 객체 자체가 되므로 2차원 배열과는 다르게 v.속성으로 표기해야 한다. */}
-
-          {/* map()을 쓸 때에는 바로 아래 child태그에 고유한 값으로 key를 입력해줘야 한다. */}
-          {/*  key는 화면에 표시는 안되지만 리액트가 key를 보고 같은 컴포넌트인지 아닌지 판단하기 때문에 성능 최적화를 위해 웬만하면 적어주는 것이 좋다. */}
-
-          {/* 리액트에서 복잡한 반복문 쓰는 법 - 반복되는 부분을 위처럼 배열 안의 객체로 만들어 사용한다.*/}
-
-          {/* 이렇게 하면 가독성이 떨어진다는 단점이 있는데, 이게 바로 props를 쓰는 이유이다. props를 쓰면 가독성 문제가 어느정도 해결된다. */}
         </ul>
       </div>
     );
