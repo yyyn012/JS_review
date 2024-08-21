4ccf4d087b285ba2d97616cb39cee9cd6d8404e8
yyyn012 <yyyn12_@naver.com>
2024-08-21 16:03:40 +0900

    Component separation

--- react-webgame/3. 숫자야구/NumberBaseball.jsx
+++ react-webgame/3. 숫자야구/NumberBaseball.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Try from "./Try";
 
 function getNumbers() {
   // 숫자 4개를 겹치지 않고 랜덤하게 뽑는 함수
@@ -16,6 +17,13 @@ class NumberBaseball extends Component {
 
   onChange = () => {};
 
+  fruit = [
+    { fruit: "apple", taste: "good" },
+    { fruit: "banana", taste: "sweet" },
+    { fruit: "grape", taste: "my favorite" },
+    { fruit: "apple", taste: "not bad" },
+  ];
+
   render() {
     return (
       <div>
@@ -29,20 +37,8 @@ class NumberBaseball extends Component {
         </form>
         <div>try : {this.state.tries.length}</div>
         <ul>
-          {[
-            { fruit: "apple", taste: "good" },
-            { fruit: "banana", taste: "sweet" },
-            { fruit: "grape", taste: "favorite" },
-            { fruit: "apple", taste: "not bad" },
-          ].map((v, i) => (
-            // 여기에서 i는 인덱스를 나타낸다. (v, 0)은 첫번째 객체를 뜻한다.
-            // i는 고유한 값(겹치지 않는 숫자)이라 종종 key에 i를 넣는 실수를 할 때가 있다.
-            // 하지만 이 경우 성능최적화에 문제가 생길 수 있기 때문에 key에는 i를 입력하지 않는 것이 좋다.
-            // (리액트에서 무엇이 바뀌었는지 인식하는 데 어려움이 있을 수 있다고 한다.)
-            <li key={v.fruit + v.taste}>
-              {/* v.fruit만 키 값으로 입력할 경우 마지막 객체에서 "apple"이 겹치기 때문에 v.fruit + v.taste를 키 값으로 입력해주었다. */}
-              <b>{v.fruit}</b>- {i}
-            </li>
+          {this.fruit.map((v, i) => (
+            <Try />
           ))}
         </ul>
       </div>
---
+++ react-webgame/3. 숫자야구/Try.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Try extends Component {
+  render() {
+    return (
+      <li>
+        <b>{v.fruit}</b> - {i}
+        <div>content 1</div>
+        <div>content 2</div>
+        <div>content 3</div>
+      </li>
+    );
+  }
+}
+
+export default Try;
