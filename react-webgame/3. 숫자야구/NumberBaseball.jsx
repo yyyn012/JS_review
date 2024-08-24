cb3102ed1e687e0c31de2c16971db858143e823f
yyyn012 <yyyn12_@naver.com>
2024-08-24 23:58:56 +0900

    Numberbaseball Component Separation

--- react-webgame/3. 숫자야구/NumberBaseball.jsx
+++ react-webgame/3. 숫자야구/NumberBaseball.jsx
@@ -3,6 +3,13 @@ import Try from "./Try";
 
 function getNumbers() {
   // 숫자 4개를 겹치지 않고 랜덤하게 뽑는 함수
+  const candidate = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const array = [];
+  for (let i = 0; i < 4; i += 1) {
+    const chosen = candidate.splice(Math.floor(Math.random() * (9 - i)), 1)[0];
+    array.push(chosen);
+  }
+  return array;
 }
 
 class NumberBaseball extends Component {
@@ -10,26 +17,72 @@ class NumberBaseball extends Component {
     result: "",
     value: "",
     answer: getNumbers(),
-    tries: [],
+    tries: [], // 배열에 값을 넣을 때 push()를 자주 쓰는데 여기서는 push()를 쓸 수 없음
+    // 리액트에서는 기존 배열을 복사해서 새로운 배열을 만든 후 써야 함.(리액트가 무엇이 바뀌었는지 감지하지 못하기 때문)
   };
 
   onSubmit = (e) => {
     e.preventDefault();
-    console.log(this.state.value);
+    if (this.state.value === this.state.answer.join("")) {
+      this.setState({
+        result: "홈런!",
+        tries: [
+          ...this.state.tries, // 예전 배열 복사
+          { try: this.state.value, result: "홈런!" }, // 복사된 배열에 새로운 값 넣어주기
+        ],
+      });
+      alert("게임을 다시 시작합니다.");
+      this.setState({
+        value: "",
+        answer: getNumbers(),
+        tries: [],
+      });
+    } else {
+      // 답을 틀렸을 경우
+      const answerArray = this.state.value.split("").map((v) => parseInt(v));
+      let strike = 0;
+      let ball = 0;
+      if (this.state.tries.length >= 9) {
+        // 10번 이상 틀렸을 때 답을 알려준 후 게임 초기화
+        this.setState({
+          result: `실패!(실패 횟수 10번 초과) 답은 ${this.state.answer.join(
+            ","
+          )}였습니다!`,
+        });
+        alert("게임을 다시 시작합니다.");
+        this.setState({
+          value: "",
+          answer: getNumbers(),
+          tries: [],
+        });
+      } else {
+        for (let i = 0; i < 4; i += 1) {
+          if (answerArray[i] === this.state.answer[i]) {
+            strike += 1;
+          } else if (this.state.answer.includes(answerArray[i])) {
+            ball += 1;
+          }
+        }
+        this.setState({
+          tries: [
+            ...this.state.tries,
+            {
+              try: this.state.value,
+              result: `${strike}, 스트라이크, ${ball} 볼`,
+            },
+          ],
+          value: "",
+        });
+      }
+    }
   };
 
   onChange = (e) => {
+    console.log(this.state.answer);
     this.setState({
       value: e.target.value,
     });
   };
-
-  fruits = [
-    { fruit: "apple", taste: "good" },
-    { fruit: "banana", taste: "sweet" },
-    { fruit: "grape", taste: "my favorite" },
-    { fruit: "apple", taste: "not bad" },
-  ];
 
   render() {
     return (
@@ -44,9 +97,8 @@ class NumberBaseball extends Component {
         </form>
         <div>try : {this.state.tries.length}</div>
         <ul>
-          {this.fruits.map((v, i) => {
-            return <Try key={v.fruit + v.taste} value={v} index={i} />;
-            // 다음과 같이 컴포넌트를 분리했을 때 props를 이용하면 쉽게 인자 전달을 할 수 있다.
+          {this.state.tries.map((v, i) => {
+            return <Try key={`${i + 1}차 시도 : `} tryInfo={v} />;
           })}
         </ul>
       </div>
--- react-webgame/3. 숫자야구/Try.jsx
+++ react-webgame/3. 숫자야구/Try.jsx
@@ -4,10 +4,8 @@ class Try extends Component {
   render() {
     return (
       <li>
-        <b>{this.props.value.fruit}</b> - {this.props.index}
-        <div>content 1</div>
-        <div>content 2</div>
-        <div>content 3</div>
+        <div>{this.props.tryInfo.try}</div>
+        <div>{this.props.tryInfo.result}</div>
       </li>
     );
   }
