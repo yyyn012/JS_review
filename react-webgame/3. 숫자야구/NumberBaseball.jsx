b2932c1eb6eb458154788501c75b5cce1c650ef8
yyyn012 <yyyn12_@naver.com>
2024-08-25 17:37:34 +0900

    Change Class to Hooks
    And lazy init

--- react-webgame/3. 숫자야구/NumberBaseball.jsx
+++ react-webgame/3. 숫자야구/NumberBaseball.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Try from "./Try";
 
 function getNumbers() {
@@ -12,32 +12,34 @@ function getNumbers() {
   return array;
 }
 
-class NumberBaseball extends Component {
-  state = {
-    result: "",
-    value: "",
-    answer: getNumbers(),
-    tries: [],
-  };
+const NumberBaseball = () => {
+  const [result, setResult] = useState("");
+  const [value, setValue] = useState("");
+  const [answer, setAnswer] = useState(getNumbers);
+  // getNumbers()로 값을 넣을 경우 겉으로 볼 때 오류는 없지만 다음과 같은 문제가 생긴다.
+  // useState는 getNumbers()의 첫번째 값을 answer에게 넘겨준다.
+  // 이후 getNumbers()는 answer에 첫번째 값이 넘어가도 리렌더링이 될 때마다 새로운 값을 만들어낸다.
+  // 다행히 useState는 getNumbers()에서 만들어지는 첫번째 이후의 값을 반복해서 넘겨주진 않는다.
+  // 따라서 오류가 뜨진 않지만 getNumbers()는 넘겨지지 않는 값을 만들어내는 쓸데없는 리렌더링을 반복하게 된다.
 
-  onSubmit = (e) => {
-    const { value, tries, answer } = this.state;
-    // 구조분해 문법으로 코드를 hooks와 비슷하게 정리할 수 있다.
+  //이와 같이 함수가 useState에 들어가는 경우는 lazy init이라고 한다.
+  //즉, 해당 함수가 호출되서 리턴 값을 돌려줄 때까지 리액트가 기다려 주는 것을 말한다.
+  const [tries, setTries] = useState([]);
+
+  const onSubmit = (e) => {
     e.preventDefault();
     if (value === answer.join("")) {
-      this.setState((prevState) => {
-        return {
-          result: "홈런!",
-          tries: [...prevState.tries, { try: value, result: "홈런!" }],
-        };
+      setResult("홈런!");
+      setTries((prevTries) => {
+        return [...prevTries, { try: value, result: "홈런!" }];
       });
-      // state를 연달아 쓸 경우 위와 같이 예전 값을 prevState(함수형 setState)로 전달하는 것이 좋다.
+
       alert("게임을 다시 시작합니다.");
-      this.setState({
-        value: "",
-        answer: getNumbers(),
-        tries: [],
-      });
+
+      setValue("");
+      setAnswer(getNumbers());
+      //여기서는 함수를 호출해서 써야 하는 자리이기 때문에 getNumbers()를 써야 한다. (위의 useState 경우처럼 ()를 없애면 안 됨.)
+      setTries([]);
     } else {
       // 답을 틀렸을 경우
       const answerArray = value.split("").map((v) => parseInt(v));
@@ -45,17 +47,14 @@ class NumberBaseball extends Component {
       let ball = 0;
       if (tries.length >= 9) {
         // 10번 이상 틀렸을 때 답을 알려준 후 게임 초기화
-        this.setState({
-          result: `실패!(실패 횟수 10번 초과) 답은 ${answer.join(
-            ","
-          )}였습니다!`,
-        });
+        setResult(
+          `실패!(실패 횟수 10번 초과) 답은 ${answer.join(",")}였습니다!`
+        );
         alert("게임을 다시 시작합니다.");
-        this.setState({
-          value: "",
-          answer: getNumbers(),
-          tries: [],
-        });
+
+        setValue("");
+        setAnswer(getNumbers());
+        setTries([]);
       } else {
         for (let i = 0; i < 4; i += 1) {
           if (answerArray[i] === answer[i]) {
@@ -64,46 +63,37 @@ class NumberBaseball extends Component {
             ball += 1;
           }
         }
-        this.setState((prevState) => {
-          return {
-            tries: [
-              ...prevState.tries,
-              {
-                try: value,
-                result: `${strike}, 스트라이크, ${ball} 볼`,
-              },
-            ],
-            value: "",
-          };
-        });
+
+        setTries((prevTries) => [
+          ...prevTries,
+          {
+            try: value,
+            result: `${strike}, 스트라이크, ${ball} 볼`,
+          },
+        ]);
+        setValue("");
       }
     }
   };
 
-  onChange = (e) => {
-    console.log(this.state.answer);
-    this.setState({
-      value: e.target.value,
-    });
+  const onChange = (e) => {
+    setValue(e.target.value);
   };
 
-  render() {
-    const { result, value, tries } = this.state;
-    return (
-      <div>
-        <h1>{result}</h1>
-        <form onSubmit={this.onSubmit}>
-          <input maxLength={4} value={value} onChange={this.onChange} />
-        </form>
-        <div>try : {tries.length}</div>
-        <ul>
-          {tries.map((v, i) => {
-            return <Try key={`${i + 1}차 시도 : `} tryInfo={v} />;
-          })}
-        </ul>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <h1>{result}</h1>
+      <form onSubmit={onSubmit}>
+        <input maxLength={4} value={value} onChange={onChange} />
+      </form>
+      <div>try : {tries.length}</div>
+      <ul>
+        {tries.map((v, i) => {
+          return <Try key={`${i + 1}차 시도 : `} tryInfo={v} />;
+        })}
+      </ul>
+    </div>
+  );
+};
 
 export default NumberBaseball;
--- react-webgame/3. 숫자야구/Try.jsx
+++ react-webgame/3. 숫자야구/Try.jsx
@@ -1,14 +1,24 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Try extends Component {
-  render() {
-    return (
-      <li>
-        <div>{this.props.tryInfo.try}</div>
-        <div>{this.props.tryInfo.result}</div>
-      </li>
-    );
-  }
-}
+const Try = ({ tryInfo }) => {
+  return (
+    <li>
+      <div>{tryInfo.try}</div>
+      <div>{tryInfo.result}</div>
+    </li>
+  );
+};
+// 위와 같이 props자리에 tryInfo를 구조분해 문법으로 써주는 것이 깔끔하다.(실제로도 이 방법이 더 많이 쓰임)
+
+// 구조분해 문법을 쓰지 않은 오리지널 코드는 다음과 같다.
+
+// const Try = (props) => {
+// return(
+// <li>
+// <div>{props.tryInfo.try}</div>
+// <div>{props.tryInfo.result}</div>
+// </li>
+// )
+// }
 
 export default Try;
