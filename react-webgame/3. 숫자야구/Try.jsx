9fa5e403c092aaf8a71820f9057263395d8e0877
yyyn012 <yyyn12_@naver.com>
2024-08-27 19:54:23 +0900

    Connecting 'state' and 'props' (3)
    Setting the value of child props in class.( + constructor)

--- react-webgame/3. 숫자야구/Try.jsx
+++ react-webgame/3. 숫자야구/Try.jsx
@@ -27,10 +27,21 @@
 import React, { PureComponent } from "react";
 
 class Try extends PureComponent {
-  state = {
-    result: this.props.result,
-    try: this.props.try,
-  };
+  constructor(props) {
+    super(props);
+    // constructor를 쓰면 (constructor가 함수이기 때문에) 기본 객체들로는 실행불가능한 미세한 동작들을 설정할 수 있다.
+    // 꼭 constructor가 아니더라도 그때 그때 상황에 맞춰 Ref(), setState()등을 이용할 수도 있다.
+    // 예를 들어 다음과 같은 console.log나 변수 선언이나 필터링 등을 말한다.
+
+    const filtered = this.props.filter(() => {
+      //예시
+    });
+
+    state = {
+      result: filtered,
+      try: this.props.try,
+    };
+  }
 
   render() {
     const { tryInfo } = this.props;
