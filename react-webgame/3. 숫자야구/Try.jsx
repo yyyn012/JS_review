f9c6339e021b5c3ea46bde55965a3867a6789024
yyyn012 <yyyn12_@naver.com>
2024-08-27 19:30:14 +0900

    Connecting 'state' and 'props' (2)
    Setting child 'props' values (in class code)

--- react-webgame/3. 숫자야구/Try.jsx
+++ react-webgame/3. 숫자야구/Try.jsx
@@ -1,22 +1,46 @@
-import React, { memo, useState } from "react";
+// import React, { memo, useState } from "react";
 
-const Try = memo(({ tryInfo }) => {
-  const [result, setResult] = useState(tryInfo.result);
-  // 위와 같은 코드가 좋은 코드는 아니지만 가끔 실무에서 어쩔 수 없이 쓰는 경우가 있다.
-  // (props는 부모 쪽에서 바꾸는 것이 원칙이다.)
+// const Try = memo(({ tryInfo }) => {
+//   const [result, setResult] = useState(tryInfo.result);
 
-  const onClick = () => {
-    setResult("1");
+//   const onClick = () => {
+//     setResult("1");
+//   };
+
+//   return (
+//     <li>
+//       <div>{tryInfo.try}</div>
+//       <div onClick={onClick}>{result}</div>
+//     </li>
+//   );
+// });
+
+// Try.displayName = "Try";
+
+// export default Try;
+
+//
+
+//
+
+//아래는 class에서 자식 props의 값을 설정하는 코드이다.
+import React, { PureComponent } from "react";
+
+class Try extends PureComponent {
+  state = {
+    result: this.props.result,
+    try: this.props.try,
   };
 
-  return (
-    <li>
-      <div>{tryInfo.try}</div>
-      <div onClick={onClick}>{result}</div>
-    </li>
-  );
-});
-
-Try.displayName = "Try";
+  render() {
+    const { tryInfo } = this.props;
+    return (
+      <li>
+        <div>{tryInfo.try}</div>
+        <div onClick={onClick}>{result}</div>
+      </li>
+    );
+  }
+}
 
 export default Try;
