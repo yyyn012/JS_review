9f94c24dbfdf6aac86a1b2b81ce639432ac0e714
yyyn012 <yyyn12_@naver.com>
2024-08-28 15:26:32 +0900

    package.json & webpack.config.js
    package-lock.json & client.jsx & webpack-dev-server

---
+++ react-webgame/4. 반응속도체크/webpack.config.js
@@ -0,0 +1,56 @@
+const path = require("path");
+const RefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+const webpack = require("webpack");
+
+module.exports = {
+  mode: "development",
+  devtool: "eval",
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  entry: {
+    app: ["./client"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.[jt]sx?$/,
+        loader: "babel-loader",
+        options: {
+          presets: [
+            [
+              "@babel/preset-env",
+              {
+                targets: {
+                  browsers: ["> 1% in KR"],
+                },
+                debug: true,
+              },
+            ],
+            "@babel/preset-react",
+          ],
+          plugins: [
+            "@babel/plugin-transform-class-preperties",
+            "@babel/plugin-transform-private-methods",
+          ],
+        },
+      },
+    ],
+  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new RefreshWebpackPlugin(),
+  ],
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "app.js",
+    publicPath: "/dist/",
+  },
+  devServer: {
+    devMiddleware: { publicPath: "/dist" },
+    static: {
+      directory: path.resolve(__dirname),
+    },
+  },
+};
