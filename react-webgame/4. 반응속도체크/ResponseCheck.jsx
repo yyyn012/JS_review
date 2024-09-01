e49f7861e6d68fdb6907fcdc800c20e00a8d2705
yyyn012 <yyyn12_@naver.com>
2024-09-01 22:26:05 +0900

    renderAverage & onClickScreen

--- react-webgame/4. 반응속도체크/ResponseCheck.jsx
+++ react-webgame/4. 반응속도체크/ResponseCheck.jsx
@@ -7,23 +7,54 @@ class ResponseCheck extends Component {
     result: [],
   };
 
-  onClickScreen = () => {};
+  onClickScreen = () => {
+    const { state, message, result } = this.state;
+    if (state === "waiting") {
+      // state가 'waiting' 상태일 경우
+      this.setState({
+        state: "ready",
+        message: "초록색이 되면 클릭하세요",
+      });
+      setTimeout(() => {
+        this.setState({
+          state: "now",
+          message: "지금 클릭",
+        });
+      }, Math.floor(Math.random() * 1000) + 2000); // 2초~3초 랜덤 시작
+    } else if (state === "ready") {
+      // 시작 전에 클릭했을 경우
+      this.setState({
+        state: "waiting",
+        message: "너무 성급하시군요! 초록색이 된 후에 클릭하세요",
+      });
+    } else if (state === "now") {
+      // 반응속도 체크 시작
+      this.setState({
+        state: "waiting",
+        message: "클릭해서 시작하세요.",
+        result: [],
+      });
+    }
+  };
+
+  renderAverage = () => {
+    const { result } = this.state;
+    return result.length === 0 ? null : (
+      <div>
+        평균 시간 :{result.reduce((a, c) => a + c) / result.length}
+        ms
+      </div>
+    );
+  };
 
   render() {
+    const { state, message } = this.state;
     return (
       <div>
-        <div
-          id="screen"
-          className={this.state.state}
-          onClick={this.onClickScreen}
-        >
-          {this.state.message}
+        <div id="screen" className={state} onClick={this.onClickScreen}>
+          {message}
         </div>
-        <div>
-          평균 시간 :
-          {this.state.result.reduce((a, c) => a + c) / this.state.result.length}
-          ms
-        </div>
+        {this.renderAverage()}
       </div>
     );
   }
